e08cefb55a496648b0f5c9035e307d926dee18f8
Gude <S566615@nwmissouri.edu>
2023-11-01 17:33:48 -0500

    Pulling in images

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var elephantRouter = require('./routes/elephant');
+var boardRouter = require('./routes/board');
+var chooseRouter = require('./routes/choose');
 
 var app = express();
 
@@ -23,7 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/elephant',elephantRouter)
-
+app.use('/board',boardRouter)
+app.use('/choose',chooseRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
